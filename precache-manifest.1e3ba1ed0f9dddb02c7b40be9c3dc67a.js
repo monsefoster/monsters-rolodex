c3b50e11793d50741119ddd9ff77eb786cedc56a
monse <monsefoster@gmail.com>
2020-11-06 15:45:44 +0100

    Updates

---
+++ precache-manifest.1e3ba1ed0f9dddb02c7b40be9c3dc67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc92579a703f8d73ef0fca02d681a3e0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4a33237737347cabaede",
+    "url": "/monsters-rolodex/static/css/main.c0d4c490.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a33237737347cabaede",
+    "url": "/monsters-rolodex/static/js/main.6dee2c02.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
